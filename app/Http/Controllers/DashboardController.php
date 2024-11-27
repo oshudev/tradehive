4aa843d501b55ed8a1cee6f3a73c79432a698f27
DJCAYZ <charlesaaron07@gmail.com>
2024-11-27 19:31:39 +0800

    fix: add freelancer_index action
    it got deleted after merging

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -18,5 +19,24 @@ class DashboardController extends Controller
         return Inertia::render('Client/Dashboard', [
             'projects' => $projects,
         ]);
+    }
+
+    public function freelancer_index() {
+        $proposals = Proposal::with('project:id,title,status')
+            ->where('freelancer_id', Auth::id())
+            ->get()
+            ->map(function ($proposal) {
+                return [
+                    'id' => $proposal->id,
+                    'project_id' => $proposal->project->id,
+                    'freelancer' => $proposal->freelancer->first_name . ' ' . $proposal->freelancer->last_name,
+                    'bid_amount' => $proposal->bid_amount,
+                    'title' => $proposal->project->title,
+                    'status' => $proposal->project->status,
+                ];
+            });
+
+        return $proposals;
+        
     }
 }
